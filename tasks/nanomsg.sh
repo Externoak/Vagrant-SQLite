76a19e4ef97427754e40cea3c5549b055abcaf6b
Darío <dariomr1234@gmail.com>
2017-02-09 18:00:57 +0100

    Update nanomsg.sh

--- tasks/nanomsg.sh
+++ tasks/nanomsg.sh
@@ -6,9 +6,6 @@ tar -xzvf nanomsg-1.0.0.tar.gz
 cd nanomsg-1.0.0
 mkdir build
 cd build
-# I added -DCMAKE_INSTALL_PREFIX=/usr because on my Ubuntu 14.04 machine, the
-# library is installed in /usr/local/lib/x86_64-linux-gnu/ by default, and for
-# some reason ldconfig cannot find it
 cmake .. -DCMAKE_INSTALL_PREFIX=/usr
 cmake --build .
 sudo cmake --build . --target install
